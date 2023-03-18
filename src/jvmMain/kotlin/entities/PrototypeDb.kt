1503bda863905c7fb6c88b54dcfd653b2ea5f0bf
George Papadopoulos <george.719pap@gmail.com>
2023-03-18 19:05:31 +0200

    todo
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

---
+++ src/jvmMain/kotlin/entities/PrototypeDb.kt
@@ -0,0 +1,27 @@
+package keb.server.entities
+
+import org.springframework.data.relational.core.mapping.Table
+
+/*
+ * A big assumption for this editor is that we will always have one unified ui.
+ */
+
+//@Table("editor")
+// class Editor
+
+@Table("workspaces")
+data class Workspace(
+    val content: String, // TODO: convert it to `Text`
+    val users: Set<User>,
+    val files: Set<File>
+)
+
+@Table("users")
+data class User(
+    val name: String
+)
+
+@Table("files")
+data class File(val content: String)
+
+data class Widget(val value: String)
---
+++ src/jvmMain/resources/db/keb.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS keb;
+USE keb;
+
+# CREATE TABLE IF NOT EXISTS accounts
+# (
+#     uuid         VARBINARY(36) NOT NULL,
+#     number       VARCHAR(30)   NOT NULL UNIQUE,
+#     identity_key VARCHAR(255) DEFAULT NULL,
+#     name         VARCHAR(30)   NOT NULL,
+#     PRIMARY KEY (uuid)
+# ) ENGINE = InnoDB
+#   DEFAULT CHARSET = utf8mb4;
