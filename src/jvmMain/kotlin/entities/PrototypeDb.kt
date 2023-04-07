f5c23cd66688e9f9510c857fd996bbd453db7235
George Papadopoulos <george.719pap@gmail.com>
2023-04-07 19:27:36 +0300

    small checkpoint2
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- src/jvmMain/kotlin/entities/PrototypeDb.kt
+++ src/jvmMain/kotlin/entities/PrototypeDb.kt
@@ -13,18 +13,18 @@ import org.springframework.data.relational.core.mapping.Table
  * Core entity to represent application's UI.
  */
 @Table("workspaces")
-data class Workspace(
-    val content: String, // TODO: convert it to `Text`
-    val users: Set<User>,
-    val files: Set<File>
+data class WorkspaceEntity(
+    val id: Int, // uuid to identify workspace
+    val users: Set<UserEntity>,
+    val files: Set<FileEntity>
 )
 
 @Table("users")
-data class User(
+data class UserEntity(
     val name: String
 )
 
 @Table("files")
-data class File(val content: String)
+data class FileEntity(val content: String)
 
-data class Widget(val value: String)
+//data class Widget(val value: String)
--- src/jvmMain/resources/db/keb.sql
+++ src/jvmMain/resources/db/keb.sql
@@ -1,12 +1,32 @@
 CREATE DATABASE IF NOT EXISTS keb;
 USE keb;
 
-# CREATE TABLE IF NOT EXISTS accounts
-# (
-#     uuid         VARBINARY(36) NOT NULL,
-#     number       VARCHAR(30)   NOT NULL UNIQUE,
-#     identity_key VARCHAR(255) DEFAULT NULL,
-#     name         VARCHAR(30)   NOT NULL,
-#     PRIMARY KEY (uuid)
-# ) ENGINE = InnoDB
-#   DEFAULT CHARSET = utf8mb4;
+CREATE TABLE IF NOT EXISTS workspace
+(
+    id mediumint not null,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS files
+(
+    name         smallint  not null,
+    content      text      not null,
+    workspace_id mediumint not null,
+    CONSTRAINT PRIMARY KEY (name),
+    CONSTRAINT FK_files_1 FOREIGN KEY (workspace_id) REFERENCES workspace (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    name         varchar(255) not null,
+    workspace_id mediumint    not null,
+    PRIMARY KEY (name),
+    CONSTRAINT FK_files_1 FOREIGN KEY (workspace_id) REFERENCES workspace (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
