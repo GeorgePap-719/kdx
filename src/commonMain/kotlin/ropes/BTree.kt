0984b26df6fc512164d18d79e3f298317511a3f0
George Papadopoulos <george.719pap@gmail.com>
2023-06-05 17:04:30 +0300

    Some clean
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- src/commonMain/kotlin/ropes/BTree.kt
+++ src/commonMain/kotlin/ropes/BTree.kt
@@ -2,16 +2,12 @@ package keb.ropes
 
 import keb.classSimpleName
 import keb.hexAddress
-import kotlin.math.min
 
 open class NodeInfo //TODO: impl later, but it seems it is not needed for btree impl
 
 /**
  * Represents a self-balancing tree node.
  */
-// Note:
-// Each leaf node contains a string (see note below) and its length, called weight. Each intermediate node also contains
-// a weight which is the sum of all the leaves in its left subtree.
 sealed class BTreeNode(
     /**
      * If this node is a leaf, then this value represents the length of the string. Else holds the sum of the lengths of
@@ -31,80 +27,6 @@ sealed class BTreeNode(
     abstract val isLegalNode: Boolean
 
     val isEmpty: Boolean = weight == 0
-//    val isRoot: Boolean = height == 0
-
-    //TODO: lol, just realized, there is no big value to add insert() onto btree itself..
-    fun insert(value: String, index: Int): BTreeNode {
-        // To insert a new element, search the tree to find the leaf node where the new element should be added
-        // TODO: handle case when insertion is in root separately? Note: wont be a regular case tho.
-        val targetNode = this[index]
-        if (targetNode == null) {
-            TODO() // append
-        }
-        if (index == 0) {
-            TODO() // prepend
-        }
-        val len = targetNode.value.length + value.length
-        val shouldSplit = len > MAX_SIZE_LEAF
-        if (shouldSplit) {
-            val newParent = splitAndMerge(targetNode, value)
-            TODO()
-        }
-
-        // case: no-split
-        val newLeaf = LeafNode(targetNode.value + value)
-        val parent = getParentOrNull(targetNode) ?: return newLeaf // this node is-root
-        // change with copy-on-write:
-        val newChildren = parent.children.replaceWithCopyOnWrite(targetNode, newLeaf)
-        val newParent = InternalNode(parent.weight, parent.height, newChildren)
-        // copy-on-write newParent
-        return copyOnWriteNewTreeNoSplit(parent, newParent)
-    }
-
-    fun concat() {
-
-    }
-
-    private fun splitAndMerge(target: LeafNode, newValue: String): InternalNode {
-        val halvedLen = (target.value.length + newValue.length) / 2
-        if (halvedLen > MAX_SIZE_LEAF) TODO()
-        TODO()
-    }
-
-    private fun copyOnWriteNewTreeNoSplit(oldParent: InternalNode, newParent: InternalNode): InternalNode {
-        val root = this as InternalNode // we take for granted that this node is not a leaf,
-        // since we check it in previous steps.
-        return copyOnWriteNewTreeNoSplitImpl(root, oldParent, newParent)
-    }
-
-    // steps:
-    // 1 -> begin search from root
-    // 2 -> find old ref of oldParent
-    // 3 -> start replacing
-    // 4 -> until root.
-    private fun copyOnWriteNewTreeNoSplitImpl(
-        curParent: InternalNode,
-        oldParent: InternalNode,
-        newParent: InternalNode
-    ): InternalNode {
-        for (node in curParent.children) {
-            when (node) {
-                is InternalNode -> {
-                    if (node === oldParent) {
-                        val newChildren = curParent.children.replaceWithCopyOnWrite(node, newParent)
-                        //a bit bad naming
-                        val newCurParent = InternalNode(curParent.weight, curParent.height, newChildren)
-                        val parent = getParentOrNull(newCurParent) ?: return newCurParent // current node is-root
-                        return copyOnWriteNewTreeNoSplitImpl(parent, curParent, newCurParent)
-                    }
-                    return copyOnWriteNewTreeNoSplitImpl(node, oldParent, newParent)
-                }
-
-                is LeafNode -> continue
-            }
-        }
-        error("cannot reach here..") //TODO: refactor this, so it won't be needed
-    }
 
     /**
      * Returns the parent node of [child], or `null` if [child] is the root node.
@@ -251,38 +173,6 @@ class LeafNode(val value: String) : BTreeNode(value.length, 0) {
     val length: Int = value.length
 
     override val isLegalNode: Boolean = length <= MAX_SIZE_LEAF
-
-    // Returns BTreeNode
-    fun pushAndMaybeSplit(other: String, index: Int): BTreeNode {
-//        if (stringFits(other)) { // no-splitting
-//            return LeafNode(value + other, height)
-//        } else { // splits
-//
-//        }
-        TODO()
-    }
-
-//    override operator fun get(index: Int): Char = value[index]
-
-    //TODO: Future improvement (prob) Try to split at newline boundary (leaning left), if not, then split at codepoint.
-    private fun split(other: String) {
-        if (true) { // isRoot
-            // find leaf to split
-            val splitPoint = min(MAX_SIZE_LEAF, length)
-            val stringToBeSplit = value + other
-            val leftString = stringToBeSplit.substring(0 until splitPoint)
-            val rightString = stringToBeSplit.substring(splitPoint)
-//            val leftNode = LeafNode()
-//            val newRoot = InternalNode(leftString.length, listOf(LeafNode(leftString, newRoot)))
-            //TODO: builder
-//            buildList<> { }
-        } else { // slow-path
-
-        }
-    }
-
-    //TODO: better name?
-    private fun stringFits(value: String): Boolean = length + value.length < MAX_SIZE_LEAF
 }
 
 const val MIN_CHILDREN = 4
@@ -365,45 +255,66 @@ fun List<BTreeNode>.addWithCopyOnWrite(newNode: List<BTreeNode>, index: Int): Li
     }
 }
 
+/**
+ * Tries to add a list of [children] on this node with copy-on-write semantics, in the specified [index].
+ * In case [index] is `null` the nodes are appended to the end of list. If receiver node cannot hold more children, it returns `null`.
+ */
+fun InternalNode.tryAddChildren(children: List<BTreeNode>, index: Int? = null): InternalNode? {
+    if (this.children.size + children.size > MAX_CHILDREN) return null
+    val newChildren = this.children.addWithCopyOnWrite(children, index ?: children.size)
+    return InternalNode(this.weight, this.height, newChildren)
+}
 
 /**
- * Reads up to 32 chunks of characters and creates a node from them. The process is repeated until the end of [input].
+ * Tries to add a child on this node with copy-on-write semantics, in the specified [index]. In case [index]
+ * is `null` the nodes are appended to the end of list. If receiver node cannot hold more children, it returns `null`.
  */
-//TODO: this is only for bulk reading
-fun read32Chunks(input: String): List<BTreeNode> { //TODO: make it private?
-    val chunks = readChunksOf64Chars(input)
-    return buildList {
-        chunks.chunked(CHUNK_NUMBER).forEach { chunks ->
-            val valueNode = chunks.joinToString(separator = "") { it }
-//            add(BTreeNode(valueNode.length, valueNode))
-            TODO()
-        }
-    }
+fun InternalNode.tryAddChild(child: BTreeNode, index: Int? = null): InternalNode? {
+    if (this.children.size + 1 > MAX_CHILDREN) return null
+    val newChildren = this.children.addWithCopyOnWrite(child, index ?: children.size)
+    return InternalNode(this.weight, this.height, newChildren)
 }
 
-private fun readChunksOf64Chars(input: String): List<String> {
-    val chunks = mutableListOf<String>()
-    val len = input.length
-    var chunkCount = 0
-    val chunkBuilder = StringBuilder()
-    for (i in 0 until len) {
-        if (chunkCount == CHUNK_SIZE) {
-            val chunk = chunkBuilder.toString()
-            chunks.add(chunk)
-            chunkBuilder.clear()
-            chunkCount = 0
-        }
-        if (i == len - 2) {
-            chunkBuilder.append(input[i])
-            val chunk = chunkBuilder.toString()
-            chunks.add(chunk)
-            break
-        }
-        chunkBuilder.append(input[i])
-        chunkCount++
+//TODO: how should we check invariants?
+//TODO: document it
+//TODO: needs re-balancing
+fun merge(nodes: List<BTreeNode>): BTreeNode {
+    if (nodes.size < MAX_CHILDREN) return unbalancedMerge(nodes)
+    val leftList = nodes.subList(0, MAX_CHILDREN)
+    val rightList = nodes.subList(MAX_CHILDREN, nodes.size)
+
+    val leftParent = unbalancedMerge(leftList)
+
+    if (rightList.size < MAX_CHILDREN) {
+        val rightParent = unbalancedMerge(rightList)
+        return unbalancedMerge(leftParent, rightParent)
     }
-    return chunks
+
+    val rightParent = merge(rightList)
+    return unbalancedMerge(leftParent, rightParent)
 }
 
-const val CHUNK_NUMBER = 32
-const val CHUNK_SIZE = 64
+private fun unbalancedMerge(left: BTreeNode, right: BTreeNode): InternalNode {
+    return unbalancedMerge(listOf(left, right))
+}
+
+/**
+ * Creates a parent for [nodes], without checking if satisfies the requirements for a legal btree.
+ */
+private fun unbalancedMerge(nodes: List<BTreeNode>): InternalNode {
+    val weight = nodes.first().computeWeightInLeftSubtree()
+    val height = nodes.maxOf { it.height } + 1
+    return InternalNode(weight, height, nodes)
+}
+
+//TODO: this can probably be improved, by not searching all leaves
+private fun BTreeNode.computeWeightInLeftSubtree(): Int {
+    return when (this) {
+        is InternalNode -> {
+            val leftmost = this.children.first()
+            return leftmost.sumOf { it.weight } // sumOf weight in leaves
+        }
+
+        is LeafNode -> this.weight
+    }
+}
