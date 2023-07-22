9b8302b33d77709ce01e75fab943ca3b986dacf0
George Papadopoulos <george.719pap@gmail.com>
2023-07-22 11:13:37 +0300

    Update keb.sql
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-server/src/main/resources/db/keb.sql
+++ keb-server/src/main/resources/db/keb.sql
@@ -1,19 +1,22 @@
 CREATE DATABASE IF NOT EXISTS keb;
 USE keb;
 
-CREATE TABLE IF NOT EXISTS document_file
-(
-    id          mediumint auto_increment not null,
-    document_id mediumint                not null,
-    fileAddress mediumtext               not null,
-    PRIMARY KEY (id)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
 CREATE TABLE IF NOT EXISTS document
 (
     id        mediumint auto_increment not null,
     text_rope JSON                     not null,
     PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS document_file
+(
+    id          mediumint auto_increment not null,
+    document_id mediumint                not null,
+    fileAddress mediumtext               not null,
+    PRIMARY KEY (id),
+    KEY FK_document_file (document_id),
+    CONSTRAINT FK_document_file FOREIGN KEY (id) REFERENCES document (id)
+        ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
