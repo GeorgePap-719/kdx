5dfada85fde62aa5222a6ed8aa63e0e505ab4c48
George Papadopoulos <george.719pap@gmail.com>
2023-08-12 20:15:34 +0300

    Add DeltaBuilder
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonMain/kotlin/Delta.kt
+++ keb-rope/src/commonMain/kotlin/Delta.kt
@@ -16,16 +16,68 @@ interface Delta<T : NodeInfo> {
 
     val changes: List<DeltaElement<T>>
 
-    // The total length of the base document, used for checks in some operations
+    // The total length of the base document,
+    // used for checks in some operations.
     val baseLen: Int
 }
 
-sealed class DeltaElement<T : NodeInfo>
+sealed class DeltaElement<out T : NodeInfo>
 
 // Represents a range of text in the base document.
 class Copy(val startIndex: Int, val endIndex: Int /*`endIndex` exclusive*/) : DeltaElement<Nothing>()
-class Insert<T : NodeInfo>(input: BTreeNode<T>) : DeltaElement<T>()
+class Insert<T : NodeInfo>(val input: BTreeNode<T>) : DeltaElement<T>()
 
-class DeltaRope(override val changes: List<DeltaElement<RopeLeaf>>, override val baseLen: Int) : Delta<RopeLeaf> {
-    //TODO:
+open class DeltaSupport<T : NodeInfo>(
+    override val changes: List<DeltaElement<T>>,
+    override val baseLen: Int
+) : Delta<T>
+
+fun <T : LeafInfo> buildDelta(baseLen: Int, action: DeltaBuilder<T>.() -> Unit): Delta<T> {
+    val builder = DeltaBuilder<T>(baseLen)
+    builder.action()
+    return builder.build()
 }
+
+class DeltaBuilder<T : LeafInfo> internal constructor(baseLen: Int) {
+    private var delta: Delta<T> = DeltaSupport(listOf(), baseLen)
+    private var lastOffset = 0
+
+    fun delete(range: IntRange) {
+        val start = range.first
+        val end = delta.baseLen
+        // intervals not properly sorted
+        assert { start >= lastOffset }
+        if (start > lastOffset) addIntoDeltaElements(Copy(lastOffset, start))
+        lastOffset = end
+    }
+
+    // A bit of weird signature,
+    // even though everything is `T`
+    // here we acknowledge the fact that t is a [Rope].
+    // Fix: cannot work like it was,
+    // cause of generics doing their work properly.
+    fun replace(range: IntRange, node: BTreeNode<T>) {
+        delete(range)
+        if (node.isEmpty) addIntoDeltaElements(Insert(node))
+    }
+
+    fun isEmpty(): Boolean {
+        return lastOffset == 0 && delta.changes.isEmpty()
+    }
+
+    fun build(): Delta<T> {
+        if (lastOffset < delta.baseLen) addIntoDeltaElements(Copy(lastOffset, delta.baseLen))
+        return delta
+    }
+
+    private fun addIntoDeltaElements(element: DeltaElement<T>) {
+        val newChanges = delta.changes.plus(element)
+        delta = DeltaSupport(newChanges, delta.baseLen)
+    }
+}
+
+
+private typealias DeltaRopeNode = Delta<RopeLeaf>
+
+class DeltaRope(override val changes: List<DeltaElement<RopeLeaf>>, override val baseLen: Int) : DeltaRopeNode
+
