fec2c1d1fe80092edf67acd14f12021934551e9d
George Papadopoulos <george.719pap@gmail.com>
2023-08-18 18:04:40 +0300

    Add deletesFromUnionBeforeIndex
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonMain/kotlin/internal/Sets.kt
+++ keb-rope/src/commonMain/kotlin/internal/Sets.kt
@@ -1,8 +1,10 @@
 package keb.ropes.internal
 
 /**
- * Returns an iterator that visits the elements representing the symmetric difference,
+ * Returns an [Iterable] that visits the elements representing the symmetric difference,
  * i.e., the elements that are in `this` or in `other` but not in both, in ascending order.
+ *
+ * @throws IllegalArgumentException if the provided sets are of different sizes.
  */
 internal fun <T> Set<T>.symmetricDifference(other: Set<T>): SymmetricDifference<T> {
     require(this.size == other.size) { "cannot produce symmetric difference for sets of different size" }
@@ -14,31 +16,35 @@ internal fun <T> Set<T>.symmetricDifference(other: Set<T>): SymmetricDifference<
 internal class SymmetricDifference<T>(
     private val thisIterator: Iterator<T>,
     private val otherIterator: Iterator<T>,
-) : Iterator<T> {
-    private var _index = 2
-    private val index get() = _index.mod(2)
+) : Iterable<T> {
+    override fun iterator(): Iterator<T> {
+        return object : Iterator<T> {
+            private var _index = 2
+            private val index get() = _index.mod(2)
 
-    private fun getIndexAndIncrease(): Int {
-        val cur = index
-        _index++
-        return cur
-    }
+            private fun getIndexAndIncrease(): Int {
+                val cur = index
+                _index++
+                return cur
+            }
 
-    override fun hasNext(): Boolean = thisIterator.hasNext()
+            override fun hasNext(): Boolean = thisIterator.hasNext()
 
-    override fun next(): T = when (getIndexAndIncrease()) {
-        0 -> {
-            val next = thisIterator.next()
-            otherIterator.next()
-            next
+            override fun next(): T = when (getIndexAndIncrease()) {
+                0 -> {
+                    val next = thisIterator.next()
+                    otherIterator.next()
+                    next
+                }
+
+                1 -> {
+                    val next = otherIterator.next()
+                    thisIterator.next()
+                    next
+                }
+
+                else -> error("unexpected")
+            }
         }
-
-        1 -> {
-            val next = otherIterator.next()
-            thisIterator.next()
-            next
-        }
-
-        else -> error("unexpected")
     }
 }
