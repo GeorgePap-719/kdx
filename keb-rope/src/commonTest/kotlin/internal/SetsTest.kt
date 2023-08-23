96f9e1e1b52ad6b7894138b66d9578c72ef7acb1
George Papadopoulos <george.719pap@gmail.com>
2023-08-23 17:25:24 +0300

    Add Intersection
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonTest/kotlin/internal/SetsTest.kt
+++ keb-rope/src/commonTest/kotlin/internal/SetsTest.kt
@@ -25,4 +25,17 @@ class SetsTest {
         val set2 = setOf(3, 4, 5)
         assertFailsWith<IllegalArgumentException> { set1.symmetricDifference(set2) }
     }
+
+    @Test
+    fun testIntersection() {
+        val set1 = setOf(1, 2, 3)
+        val set2 = setOf(2, 4, 5)
+        val result = set1.intersection(set2).toList()
+        assertEquals(listOf(2), result)
+
+        val set3 = setOf(1, 9, 7)
+        val set4 = setOf(4, 5, 6, 7, 9)
+        val result2 = set3.intersection(set4).toList()
+        assertEquals(listOf(7, 9), result2)
+    }
 }
