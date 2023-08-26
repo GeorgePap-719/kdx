fc82364bff7d33bc98b9ea4df84c07ea278a190b
George Papadopoulos <george.719pap@gmail.com>
2023-08-26 19:25:22 +0300

    Major ref: move packages for structure
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonMain/kotlin/internal/AbstractEngine.kt
+++ keb-rope/src/commonMain/kotlin/internal/AbstractEngine.kt
@@ -1,9 +1,80 @@
 package keb.ropes.internal
 
 import keb.ropes.*
-import keb.ropes.ot.*
 
 internal abstract class AbstractEngine : MutableEngine {
+
+    override fun rebase(
+        expandBy: MutableList<Pair<FullPriority, Subset>>,
+        ops: List<DeltaOp>,
+        // Kind of tricky parameter,
+        // in the original implementation
+        // author passes this as `mut`
+        // but when calling the function,
+        // the variable is a `let` (immutable).
+        // From semantics perspective,
+        // it does not make much sense
+        // to mutate this variable directly
+        // on the existing item in the collection.
+        // As we use it for constructing the new (rebased) version.
+        maxUndoSoFar: Int
+    ): RebasedResult {
+        val appRevisions: MutableList<Revision> = ArrayList(ops.size)
+        var nextExpandBy: MutableList<Pair<FullPriority, Subset>> = ArrayList(expandBy.size)
+        for (op in ops) {
+            var inserts: InsertDelta<RopeLeaf>? = null
+            var deletes: Subset? = null
+            val fullPriority = FullPriority(op.priority, op.id.sessionId)
+            for ((transformPriority, transformInserts) in expandBy) {
+                // Should never be ==
+                assert { fullPriority.compareTo(transformPriority) != 0 }
+                val after = fullPriority >= transformPriority
+                // d-expand by other
+                inserts = op.inserts.transformExpand(transformInserts, after)
+                // trans-expand other by expanded so they have the same context
+                val inserted = inserts.getInsertedSubset()
+                val newTransformInserts = transformInserts.transformExpand(inserted)
+                // The `deletes` are already after our inserts,
+                // but we need to include the other inserts.
+                deletes = op.deletes.transformExpand(newTransformInserts)
+                // On the next step,
+                // we want things in `expandBy`
+                // to have `op` in the context.
+                nextExpandBy.add(transformPriority to newTransformInserts)
+            }
+            check(inserts != null)
+            check(deletes != null)
+            val textInserts = inserts.transformShrink(deletesFromUnion)
+            val textWithInserts = textInserts.applyTo(text)
+            val inserted = inserts.getInsertedSubset()
+
+            val expandedDeletesFromUnion = deletesFromUnion.transformExpand(inserted)
+            val newDeletesFromUnion = expandedDeletesFromUnion.union(deletes)
+            val (newText, newTombstones) = shuffle(
+                textWithInserts,
+                tombstones,
+                expandedDeletesFromUnion,
+                newDeletesFromUnion
+            )
+
+            trySetText(newText)
+            trySetTombstones(newTombstones)
+            trySetDeletesFromUnion(newDeletesFromUnion)
+
+            val maxUndoSoFarAfterRebased = maxOf(maxUndoSoFar, op.undoGroup)
+            appRevisions.add(
+                Revision(
+                    id = op.id,
+                    maxUndoSoFar = maxUndoSoFarAfterRebased,
+                    edit = Edit(op.priority, op.undoGroup, deletes, inserted)
+                )
+            )
+            expandBy.replaceAll(nextExpandBy)
+            nextExpandBy = ArrayList(expandBy.size)
+        }
+        return RebasedResult(appRevisions, text, tombstones, deletesFromUnion)
+    }
+
     override fun gc(gcGroups: Set<Int>) {
         TODO("Not yet implemented")
     }
