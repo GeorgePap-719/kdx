1a1130129b4b4720d8b619fb7ab69236f93d629c
George Papadopoulos <george.719pap@gmail.com>
2023-08-31 20:32:01 +0300

    Add MultisetTest.kt
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonMain/kotlin/Delta.kt
+++ keb-rope/src/commonMain/kotlin/Delta.kt
@@ -1,13 +1,13 @@
 package keb.ropes
 
 import keb.ropes.internal.intoInterval
-import kotlin.math.min
 
 internal typealias NodeInfo = LeafInfo
 
 /**
- * Represents changes to a document by describing the new document as a sequence of sections copied from the old document and of new inserted text.
+ * Represents changes to a document by describing the new document as a sequence of sections copied from the base document and of new inserted text.
  * Deletions are represented by gaps in the ranges copied from the old document.
+ * All the indices of the copied sections are non-decreasing.
  *
  * For example, Editing "abcd" into "acde" could be represented as:
  * `[Copy(0,1),Copy(2,4),Insert("e")]`
@@ -21,8 +21,8 @@ interface Delta<T : NodeInfo> {
     val changes: List<DeltaElement<T>>
 
     /**
-     * The total length of the **old document**.
-     * The typical use-case is to be used for validations in operations.
+     * The total length of the **base document**.
+     * It is typically used for validations in operations.
      */
     val baseLength: Int
 }
@@ -50,7 +50,7 @@ val <T : NodeInfo> Delta<T>.isIdentity: Boolean
  * is not compatible with the construction of the delta.
  */
 fun <T : NodeInfo> Delta<T>.applyTo(node: BTreeNode<T>): BTreeNode<T> {
-    assert { node.weight == baseLength }
+    assert { node.weight == baseLength } //TODO: should this be require?
     return buildBTree {
         for (element in changes) {
             when (element) {
@@ -61,8 +61,12 @@ fun <T : NodeInfo> Delta<T>.applyTo(node: BTreeNode<T>): BTreeNode<T> {
     }
 }
 
+/**
+ * "Factor" the delta into an [InsertDelta] and a [Subset] representing deletions.
+ * Applying the [InsertDelta] and then the delete, yields the same result as the original [Delta].
+ */
 /// Factor the delta into an insert-only delta and a subset representing deletions.
-/// Applying the insert then the delete yields the same result as the original delta:
+/// Applying the insert-delta then the delete, yields the same result as the original delta:
 ///
 /// ```no_run
 /// # use xi_rope::rope::{Rope, RopeInfo};
@@ -98,112 +102,7 @@ fun <T : NodeInfo> Delta<T>.factor(): Pair<InsertDelta<T>, Subset> {
         if (e1 < baseLength) add(e1, baseLength, 1)
         paddingToLength(baseLength)
     }
-    return InsertDelta(DeltaSupport(insertions, baseLength)) to subset
-}
-
-/// Synthesize a delta from a "union string" and two subsets: an old set
-/// of deletions and a new set of deletions from the union. The Delta is
-/// from text to text, not union to union; anything in both subsets will
-/// be assumed to be missing from the Delta base and the new text. You can
-/// also think of these as a set of insertions and one of deletions, with
-/// overlap doing nothing. This is basically the inverse of `factor`.
-///
-/// Since only the deleted portions of the union string are necessary,
-/// instead of requiring a union string the function takes a `tombstones`
-/// rope which contains the deleted portions of the union string.
-
-// /// Notes: this assumption could be an assertion?
-/// The `from_dels` subset must be the interleaving of `tombstones` into the
-/// union string.
-///
-/// ```no_run
-/// # use xi_rope::rope::{Rope, RopeInfo};
-/// # use xi_rope::delta::Delta;
-/// # use std::str::FromStr;
-/// fn test_synthesize(d : &Delta<RopeInfo>, r : &Rope) {
-///     let (ins_d, del) = d.clone().factor();
-///     let ins = ins_d.inserted_subset();
-///     let del2 = del.transform_expand(&ins);
-///     let r2 = ins_d.apply(&r);
-///     let tombstones = ins.complement().delete_from(&r2);
-///     let d2 = Delta::synthesize(&tombstones, &ins, &del);
-///     assert_eq!(String::from(d2.apply(r)), String::from(d.apply(r)));
-/// }
-/// ```
-// For if last_old.is_some() && last_old.unwrap().0 <= beg
-//TODO: research the usage of this fun.
-fun <T : NodeInfo> synthesize(
-    tombstones: BTreeNode<T>,
-    fromDeletes: Subset,
-    toDeletes: Subset
-): Delta<T> {
-    val baseLen = fromDeletes.lengthAfterDelete()
-    val changes = mutableListOf<DeltaElement<T>>()
-    var x = 0
-    val oldRanges = fromDeletes.complementIterator()
-    var lastOld = oldRanges.next()
-    val mapper = fromDeletes.mapper(CountMatcher.ZERO)
-
-    val toDelsIterator = toDeletes.complementIterator()
-    // For each segment of the new text.
-    while (toDelsIterator.hasNext()) {
-        val (b, e) = toDelsIterator.next() ?: continue
-        // Fill the whole segment.
-        var beg = b
-        while (beg < e) {
-            // Skip over ranges in old text
-            // until one overlaps where we want to fill.
-            while (lastOld != null) {
-                val (ib, ie) = lastOld
-                if (ie > beg) break
-                x += ie - ib
-                lastOld = oldRanges.next()
-            }
-            // If we have a range in the old text
-            // with the character at beg,
-            // then we Copy.
-            if (lastOld != null && lastOld.first <= beg) {
-                val (ib, ie) = lastOld
-                val end = min(e, ie)
-                // Try to merge contiguous copies in the output.
-                val xbeg = beg + x - ib // "beg - ib + x" better for overflow?
-                val xend = end + x - ib // ditto
-                val lastElement = changes.lastOrNull()
-                val merged = if (lastElement is Copy && lastElement.endIndex == xbeg) {
-                    changes.replace(Copy(lastElement.startIndex, xend), lastElement)
-                    true
-                } else {
-                    false
-                }
-                if (!merged) changes.add(Copy(xbeg, xend))
-                beg = end
-            } else {
-                // If the character at `beg` isn't in the old text,
-                // then we insert.
-                // Insert up until the next old range we could copy from,
-                // or the end of this segment.
-                var end = e
-                if (lastOld != null) end = min(end, lastOld.first)
-                // Note: could try to aggregate insertions,
-                // but not sure of the win.
-                // Use the mapper to insert the corresponding section of the tombstones rope.
-                val range = mapper.docIndexToSubset(beg)..<mapper.docIndexToSubset(end)
-                val node = tombstones.subSequence(range)
-                changes.add(Insert(node))
-                beg = end
-            }
-        }
-    }
-    return DeltaSupport(changes, baseLen)
-}
-
-// Not clear-cut if we should have a non-existent item here.
-private fun <T> MutableList<T>.replace(new: T, old: T) {
-    val index = indexOf(old)
-    assert { index != -1 }
-    if (index == -1) return
-    removeAt(index)
-    add(index, new)
+    return InsertDelta(insertions, baseLength) to subset
 }
 
 /**
@@ -266,7 +165,7 @@ fun <T : NodeInfo> InsertDelta<T>.transformExpand(xform: Subset, after: Boolean)
         if (!after && y < nextIvBeg) y = nextIvBeg
     }
     if (y > b1) changes.add(Copy(b1, y))
-    return InsertDelta(DeltaSupport(changes, len))
+    return InsertDelta(changes, len)
 }
 
 
@@ -282,7 +181,7 @@ fun <T : NodeInfo> InsertDelta<T>.transformShrink(xform: Subset): InsertDelta<T>
             is Insert -> Insert(content.input)
         }
     }
-    return InsertDelta(DeltaSupport(changes, xform.lengthAfterDelete()))
+    return InsertDelta(changes, xform.lengthAfterDelete())
 }
 
 /**
@@ -300,22 +199,27 @@ fun <T : NodeInfo> InsertDelta<T>.getInsertedSubset(): Subset = buildSubset {
 
 /**
  * Represents a [Delta] that contains only insertions.
- * That is, it copies all of the `old` document in the same order.
- *
- * It is a `Deref` implementation to allow all [Delta] operations on it.
+ * That is, it copies all of the `base` document in the same order, and then new insertions follow.
  */
-class InsertDelta<T : NodeInfo>(val value: Delta<T>) : Delta<T> {
-    override val changes: List<DeltaElement<T>> = value.changes
-    override val baseLength: Int = value.baseLength
+class InsertDelta<T : NodeInfo>(override val changes: List<DeltaElement<T>>, override val baseLength: Int) : Delta<T> {
+    override fun toString(): String = "InsertDelta(changes=$changes,baseLength=$baseLength)"
 }
 
+/**
+ * Type for a sequence in a [Delta].
+ */
 sealed class DeltaElement<out T : NodeInfo>
 
-// Represents a range of text in the base document.
+/**
+ * Represents a range in the base document.
+ */
 class Copy(val startIndex: Int, val endIndex: Int /*`endIndex` exclusive*/) : DeltaElement<Nothing>() {
     override fun toString(): String = "Copy(startIndex=$startIndex,endIndex=$endIndex)"
 }
 
+/**
+ * Represents an insert in the new document.
+ */
 class Insert<T : NodeInfo>(val input: BTreeNode<T>) : DeltaElement<T>() {
     override fun toString(): String = "Insert(input=$input)"
 }
@@ -365,7 +269,7 @@ class DeltaBuilder<T : LeafInfo> internal constructor(baseLen: Int) {
     }
 }
 
-open class DeltaSupport<T : NodeInfo>(
+internal open class DeltaSupport<T : NodeInfo>(
     override val changes: List<DeltaElement<T>>,
     override val baseLength: Int
 ) : Delta<T> {
