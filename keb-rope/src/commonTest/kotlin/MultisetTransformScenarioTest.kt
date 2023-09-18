93a9f0bf19eeefb7906d73282dc6e2f0acfa66bc
George Papadopoulos <george.719pap@gmail.com>
2023-09-18 08:50:11 +0300

    Improve KDocs
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonTest/kotlin/MultisetTransformScenarioTest.kt
+++ keb-rope/src/commonTest/kotlin/MultisetTransformScenarioTest.kt
@@ -41,6 +41,21 @@ class MultisetTransformScenarioTest {
         )
     }
 
+    /**
+     * This test-suite tests three transformation scenarios.
+     *
+     * 1) We should be able to apply the difference of [str1] and [str2] to [simpleString] through a coordinate transformation.
+     * The [result] is "result" of this difference. More technically, [result] should contain the difference of [str1] and [str2],
+     * and we expect characters that are missing and present in both subsets to be present in [result],
+     * as well as characters that are only in [str2] (as it's the base string).
+     * 2) Applying [transformShrink] between the difference of [str1] and [str2] and the `deletes1` (the "deletions" between [str1] and the [simpleString]),
+     * and then using the resulting "transform" on the result of scenario `1.` should yield the [str2].
+     * In this scenario, "mentally" we can think of this operation as a necessary step to apply `deletes1` subset on [simpleString],
+     * if we have already applied another subset (`deletes3` in our case) on it.
+     * 3) Applying [transformUnion] on the difference of [str1] and [str2] to [simpleString] through a coordinate transformation,
+     * yields the [str2] (the base string) as a result.
+     * This happens because [transformUnion] preservers the "deletes" from both subsets.
+     */
     private fun transformCase(str1: String, str2: String, result: String) {
         val deletes1 = str1.findDeletions(simpleString)
         val deletes2 = str2.findDeletions(str1)
