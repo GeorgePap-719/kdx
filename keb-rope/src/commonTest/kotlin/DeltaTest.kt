9bd11796fa69e6b1d430eb9bfab42001cd1bab26
George Papadopoulos <george.719pap@gmail.com>
2023-09-18 18:35:39 +0300

    Add tests for getInsertedSubset
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonTest/kotlin/DeltaTest.kt
+++ keb-rope/src/commonTest/kotlin/DeltaTest.kt
@@ -10,7 +10,6 @@ class DeltaTest {
     @Test
     fun testSimpleEdit() {
         val delta = simpleEdit(1..<9, Rope("era").root, 11)
-        println(delta)
         val str = delta.applyToString("hello world")
         assertEquals("herald", str.toString())
         assertEquals(6, str.length)
@@ -22,9 +21,25 @@ class DeltaTest {
         val (inserts, deletes) = delta.factor()
         assertEquals("heraello world", inserts.applyToString("hello world").toString())
         assertEquals("hld", deletes.deleteFromString("hello world"))
-        //
+    }
+
+    @Test
+    fun testFactorCanYieldSameResult() {
+        val delta = simpleEdit(1..<9, Rope("era").root, 11)
+        val (inserts, deletes) = delta.factor()
         val transform = deletes.transformExpand(inserts.getInsertedSubset())
         assertEquals("herald", transform.deleteFromString("heraello world"))
+    }
+
+    @Test
+    fun testGetInsertedSubset() {
+        val delta = simpleEdit(1..<9, Rope("era").root, 11)
+        val (inserts, _) = delta.factor()
+        assertEquals("hello world", inserts.getInsertedSubset().deleteFromString("heraello world"))
+    }
+
+    fun testSynthesize() {
+
     }
 }
 
