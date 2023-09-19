e2226b0fe82d541a885fd9a9a2b66fc791e521b6
George Papadopoulos <george.719pap@gmail.com>
2023-09-19 20:54:06 +0300

    Add comments
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- keb-rope/src/commonTest/kotlin/SynthesizeTest.kt
+++ keb-rope/src/commonTest/kotlin/SynthesizeTest.kt
@@ -11,10 +11,14 @@ class SynthesizeTest {
         // delta: "herald"
         // Break down delta into parts.
         val (inserts, deletes) = delta.factor()
+        // Subset([Segment(length=1, count=0), Segment(length=3, count=1), Segment(length=10, count=0)])
         val insertedSubset = inserts.getInsertedSubset()
+        println("insertedSubset:$insertedSubset")
         val deletesTransform = deletes.transformExpand(insertedSubset)
         val union = inserts.applyToString(str)
         println("unionString:$union")
+        // Applying complement() on a subset which represents the insertions,
+        // and then applying that subset will yield the inserted characters.
         val tombstones = insertedSubset.complement().deleteFromString(union.toString())
         println("tombstones:${tombstones}")
         // Reconstruct delta.
