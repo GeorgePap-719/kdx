bc1c72a5cf644309e769566344a24ce67b27cadf
George Papadopoulos <george.719pap@gmail.com>
2023-10-09 11:11:24 +0300

    Make computeTransforms return a read-only list
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- kdx-core/src/commonMain/kotlin/MergeSupport.kt
+++ kdx-core/src/commonMain/kotlin/MergeSupport.kt
@@ -109,31 +109,27 @@ class RebaseResult(
  * substantially for normal editing patterns. Any large runs of typing in the same place by the same user (e.g. typing a paragraph)
  * will be combined into a single segment in a transform as opposed to thousands of revisions.
  */
-//TODO: probably it is redundant to return a mutable list here.
-fun computeTransforms(revisions: List<Revision>): MutableList<Pair<FullPriority, Subset>> {
-    val list = buildList<Pair<FullPriority, Subset>> {
-        var lastPriority: Int? = null
-        for (revision in revisions) {
-            when (val content = revision.edit) {
-                is Edit -> {
-                    if (content.inserts.isEmpty()) continue
-                    // Optimization for combining sequential revisions with the same priority.
-                    if (lastPriority != null && lastPriority == content.priority) {
-                        val last = last()
-                        val newLast = last.first to last.second.transformUnion(content.inserts)
-                        this[this.lastIndex] = newLast
-                    } else {
-                        lastPriority = content.priority
-                        val priority = FullPriority(content.priority, revision.id.sessionId)
-                        add(priority to content.inserts)
-                    }
+fun computeTransforms(revisions: List<Revision>): List<Pair<FullPriority, Subset>> = buildList {
+    var lastPriority: Int? = null
+    for (revision in revisions) {
+        when (val content = revision.edit) {
+            is Edit -> {
+                if (content.inserts.isEmpty()) continue
+                // Optimization for combining sequential revisions with the same priority.
+                if (lastPriority != null && lastPriority == content.priority) {
+                    val last = last()
+                    val newLast = last.first to last.second.transformUnion(content.inserts)
+                    this[this.lastIndex] = newLast
+                } else {
+                    lastPriority = content.priority
+                    val priority = FullPriority(content.priority, revision.id.sessionId)
+                    add(priority to content.inserts)
                 }
-
-                is Undo -> continue
             }
+
+            is Undo -> continue
         }
     }
-    return list.toMutableList()
 }
 
 /**
